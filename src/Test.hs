5fd7504ac6ca67db3271daaf0a0500517c69f099
Lyrexes <51889103+Lyrexes@users.noreply.github.com>
2022-06-20 18:06:59 +0200

    Delete Test.hs

--- src/Test.hs
+++
@@ -1,5 +0,0 @@
-
-data PieceType =    PawnW | KnightW | BishopW | RookW |
-                    QueenW | KingW | PawnB | KnightB |
-                    BishopB | RookB | QueenB | KingB
-    deriving(Show, Eq, Enum)
